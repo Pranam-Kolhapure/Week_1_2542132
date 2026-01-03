32b1123b62447cec3ac88c505ea56fedab71a102
Pranam-Kolhapure <pranamkolhapure@gmail.com>
2026-01-03 20:44:18 +0530

    Add files via upload

---
+++ Calculate_area.py
@@ -0,0 +1,18 @@
+def circle(r):
+    return 3.14*r*r
+
+def triangle(b,h):
+    return(b*h)/2
+
+def square(s):
+    return s*s
+
+def rectangle(l,w):
+    return l*w
+
+print("Circle area:",circle(5))
+print("Triangle area:",triangle(4,6))
+print("Square area:",square(4))
+print("Rectangle area:",rectangle(5,3))
+
+
